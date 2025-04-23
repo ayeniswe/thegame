9575526a962cf8f7d2af8d13685100e51000258d
XxJesusIsKingxX <nazaritee@pm.me>
2025-04-22 22:45:51 -0500

    feat: basic game input handler

--- src/input.rs
+++ src/input.rs
@@ -1,32 +1,96 @@
+use crossbeam::channel::{unbounded, SendError, Sender};
 use crossterm::event::{read, Event, KeyCode};
+use log::{debug, error, info, warn};
+use std::{collections::HashMap, sync::Arc, thread};
 use thiserror::Error;
 
-use crate::Coordinate;
+use crate::{layout::Coordinate, sync::Subscriber};
 
-#[derive(Debug, Error)]
-pub enum InputError {
-    #[error("failed to read input event: {0}")]
-    ReadError(#[from] std::io::Error),
-    #[error("unhandled key input")]
-    UnhandledKey,
-    #[error("failed to read key event")]
-    ReadFailure,
+/// Centralizes terminal gameplay key input handling, mapping keys to in-game
+/// actions and broadcasting them to all subscribed components.
+pub(crate) struct GameInputHandler {
+    mapping: HashMap<GameInput, KeyCode>,
+    subscribers: Vec<Sender<Coordinate>>,
 }
-
-/// Handles terminal key input and maps directional keys to coordinate movement.
-pub(crate) struct InputHandler;
-impl InputHandler {
-    pub(crate) fn handler() -> Result<Coordinate, InputError> {
-        if let Event::Key(e) = read()? {
-            match e.code {
-                KeyCode::Left => Ok(Coordinate { x: -1.0, y: 0.0 }),
-                KeyCode::Right => Ok(Coordinate { x: 1.0, y: 0.0 }),
-                KeyCode::Up => Ok(Coordinate { x: 0.0, y: -1.0 }),
-                KeyCode::Down => Ok(Coordinate { x: 0.0, y: 1.0 }),
-                _ => Err(InputError::UnhandledKey),
+impl GameInputHandler {
+    /// Spawns a background thread to listen for key events and publish movement coordinates.
+    /// Enables decoupled, real-time input handling for interactive components.
+    pub(crate) fn start(self: Arc<Self>) {
+        thread::spawn(move || loop {
+            match read() {
+                Ok(Event::Key(event)) => {
+                    let coordinate = match event.code {
+                        KeyCode::Left => Coordinate { x: -1.0, y: 0.0 },
+                        KeyCode::Right => Coordinate { x: 1.0, y: 0.0 },
+                        KeyCode::Up => Coordinate { x: 0.0, y: -1.0 },
+                        KeyCode::Down => Coordinate { x: 0.0, y: 1.0 },
+                        _ => continue,
+                    };
+                    debug!("{:?}", coordinate);
+                    
+                    if let Err(e) = self.publish(coordinate) {
+                        error!("{}", e);
+                    }
+                }
+                Ok(_) => {
+                    warn!("{}", GameInputError::NonKeyEvent);
+                }
+                Err(e) => {
+                    error!("{}", GameInputError::IOReadError(e));
+                }
             }
-        } else {
-            Err(InputError::ReadFailure)
+        });
+    }
+    pub(crate) fn subscribe(&mut self, subscriber: &mut dyn Subscriber<Coordinate>) {
+        let (tx, rx) = unbounded::<Coordinate>();
+        self.subscribers.push(tx);
+        subscriber.subscribe(rx);
+    }
+    pub(crate) fn publish(&self, coordinate: Coordinate) -> Result<(), GameInputError> {
+        for sub in &self.subscribers {
+            sub.send(coordinate)?
+        }
+        Ok(())
+    }
+    pub(crate) fn get_keymap(&self, input: &GameInput) -> &KeyCode {
+        self.mapping.get(input).unwrap()
+    }
+    pub(crate) fn update_keymap(&mut self, input: &GameInput, key: KeyCode) {
+        *self.mapping.get_mut(input).unwrap() = key
+    }
+}
+impl Default for GameInputHandler {
+    fn default() -> Self {
+        Self {
+            mapping: [
+                (GameInput::PlayerMoveUp, KeyCode::Up),
+                (GameInput::PlayerMoveLeft, KeyCode::Left),
+                (GameInput::PlayerMoveRight, KeyCode::Right),
+                (GameInput::PlayerMoveDown, KeyCode::Down),
+            ]
+            .into(),
+            subscribers: Vec::default(),
         }
     }
+}
+
+/// Stores a comprehensive list of all input actions
+#[derive(PartialEq, Eq, Hash)]
+pub(crate) enum GameInput {
+    PlayerMoveUp,
+    PlayerMoveLeft,
+    PlayerMoveRight,
+    PlayerMoveDown,
+}
+
+#[derive(Debug, Error)]
+enum GameInputError {
+    #[error("unable to read input event (IO failure): {0}")]
+    IOReadError(#[from] std::io::Error),
+    #[error("unable to send key input update to one or more subscribers: {0}")]
+    GameInputBroadcastError(#[from] SendError<Coordinate>),
+    #[error("received key input with no associated action")]
+    NoKeyAction,
+    #[error("received non-key event")]
+    NonKeyEvent,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,84 +4,8 @@ mod layout;
 mod palette;
 mod renderer;
 mod sprite;
+mod sync;
 
-use crossterm::{
-    cursor::Hide,
-    execute,
-    terminal::{
-        disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen,
-        LeaveAlternateScreen,
-    },
-};
-use input::InputHandler;
-use layout::{Coordinate, Mirrorable};
-use ratatui::{
-    backend::CrosstermBackend,
-    layout::Rect,
-    style::{Color, Style},
-    widgets::Block,
-    Terminal,
-};
-use sprite::character::{character::Character as _, knight};
-use std::{
-    io::{stdout, Result},
-    sync::{mpsc::channel, Arc, Mutex},
-    thread::{self},
-    time::{Duration, Instant},
-};
-
-use animator::Animation;
-
-fn main() -> Result<()> {
-    enable_raw_mode()?;
-    execute!(stdout(), EnterAlternateScreen, Clear(ClearType::All), Hide)?;
-    let backend = CrosstermBackend::new(stdout());
-    let mut terminal = Terminal::new(backend)?;
-
-    // input handling
-    let (tx, rx) = channel::<Coordinate>();
-    thread::spawn(move || loop {
-        if let Ok(input) = InputHandler::handler() {
-            let _ = tx.send(input);
-        }
-    });
-
-    const FPS: u64 = 60;
-    const FRAME_DURATION: Duration = Duration::from_micros(1_000_000 / FPS);
-
-    let mut def = knight::Knight::new();
-    let mut last_pos = Coordinate { x: 0.0, y: 0.0 };
-    let mut delta = 0.0;
-    let mut last_update = Instant::now();
-    const SPEED: f32 = 60.0;
-    loop {
-        let start = Instant::now();
-
-        // Track new input of character movement
-        if let Ok(coor) = rx.try_recv() {
-            last_pos += coor * SPEED * delta
-        }
-
-        // Show new frame for user visuals
-        def.walk().play(
-            &mut terminal,
-            delta,
-            layout::MirrorDirection::None,
-            last_pos,
-        );
-
-        // Sleep only the remaining time of frame
-        let elapsed = start.elapsed();
-        if elapsed < FRAME_DURATION {
-            thread::sleep(FRAME_DURATION - elapsed);
-        }
-
-        let now = Instant::now();
-        delta = now.duration_since(last_update).as_secs_f32();
-        last_update = now;
-    }
-
-    disable_raw_mode()?;
-    execute!(stdout(), LeaveAlternateScreen)?;
-    Ok(())
+fn main() {
+    todo!()
 }
